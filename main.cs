c7459b2a7fc51653ba34c0255f2017a9179c8e11
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-27 23:59:23 +0000

    changed spacing

--- main.cs
+++ main.cs
@@ -10,14 +10,14 @@ class Program
     public static void Main(string[] args)
     {
       int num;
-      Console.Write("Check whether a number is positive or negative:");
-      Console.ReadLine();
-      Console.Write("Input an integer : ");
-      num = Convert.ToInt32(Console.ReadLine());
-      if (num < 0)
-        Console.WriteLine("The number is negative!");
-      else
-        Console.WriteLine("The number is positive!");
-      Console.WriteLine("\nDone.");
+        Console.Write("Check whether a number is positive or negative:");
+        Console.ReadLine();
+        Console.Write("Input an integer : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num < 0)
+          Console.WriteLine("The number is negative!");
+        else
+          Console.WriteLine("The number is positive!");
+        Console.WriteLine("\nDone.");
     }
 }
