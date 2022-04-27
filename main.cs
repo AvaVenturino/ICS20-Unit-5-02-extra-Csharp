0805db3d94e17a5fc42b0327d9947df67f03bc0f
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-27 22:40:27 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+// Created by: Ava Venturino
+// Created on: April 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num;
+        Console.Write("Check whether a number is positive or negative:");
+        Console.Write("Input an integer : ");
+        num= Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+        Console.WriteLine("The number is positive!");
+        else
+        Console.WriteLine("The number is negative!");
+      
+        Console.WriteLine("\nDone.");
+       }
+    }
